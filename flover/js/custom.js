67beeae474ad5d767cc01ba54c8718090d0ed4f6
ubunturifa <jstrfaheem065@gmail.com>
2020-02-02 20:01:04 +0530

    Dynamic padding,

--- flover/js/custom.js
+++ flover/js/custom.js
@@ -115,3 +115,20 @@ $(document).ready(function() {
     });
   });
 });
+
+
+$(document).ready(function(){
+  "use strict";
+  setInterval(function(){
+    "use strict";
+    var windowHeight = $(window).height();
+    var containerHeight = $(".header-container").height();
+    var padTop = windowHeight - containerHeight;
+
+
+    $(".header-container").css({
+      "padding-top": Math.round(padTop/2) + "px",
+      "padding-bottom": Math.round(padTop/2) + "px"
+    });
+  }, 10);
+});
