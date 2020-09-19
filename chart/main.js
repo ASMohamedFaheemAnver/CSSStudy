5baa750c2eab2a058c68b2578f9fd1735b56dad2
freedom <jstrfaheem065@gmail.com>
2020-09-19 23:27:21 +0530

    Chart and xiteb,

---
+++ chart/main.js
@@ -0,0 +1,47 @@
+google.charts.load("current", { packages: ["corechart", "line"] });
+google.charts.setOnLoadCallback(drawCrosshairs);
+
+function drawCrosshairs() {
+  var data = new google.visualization.DataTable();
+  data.addColumn("number", "X");
+  data.addColumn("number", "GX");
+  data.addColumn("number", "GY");
+  data.addColumn("number", "GZ");
+
+  data.addRows([
+    [1, 37.8, 80.8, 41.8],
+    [2, 30.9, 69.5, 32.4],
+    [3, 25.4, 57, 25.7],
+    [4, 11.7, 18.8, 10.5],
+    [5, 11.9, 17.6, 10.4],
+    [6, 8.8, 13.6, 7.7],
+    [7, 7.6, 12.3, 9.6],
+    [8, 12.3, 29.2, 10.6],
+    [9, 16.9, 42.9, 14.8],
+    [10, 12.8, 30.9, 11.6],
+    [11, 5.3, 7.9, 4.7],
+    [12, 6.6, 8.4, 5.2],
+    [13, 4.8, 6.3, 3.6],
+    [14, 4.2, 6.2, 3.4],
+  ]);
+
+  var options = {
+    hAxis: {
+      title: "Time",
+    },
+    vAxis: {
+      title: "Value",
+    },
+    colors: ["#a52714", "#097138", "#ffff00"],
+    crosshair: {
+      color: "#000",
+      trigger: "selection",
+    },
+  };
+
+  var chart = new google.visualization.LineChart(
+    document.getElementById("chart_div")
+  );
+
+  chart.draw(data, options);
+}
