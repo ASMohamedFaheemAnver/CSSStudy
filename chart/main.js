4f5e93b57643edd745b289d0e68b139e44e39310
freedom <jstrfaheem065@gmail.com>
2020-09-20 00:35:45 +0530

    Connected to firebase,

--- chart/main.js
+++ chart/main.js
@@ -1,47 +1,60 @@
-google.charts.load("current", { packages: ["corechart", "line"] });
-google.charts.setOnLoadCallback(drawCrosshairs);
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+var database = firebase.database();
 
-function drawCrosshairs() {
-  var data = new google.visualization.DataTable();
-  data.addColumn("number", "X");
-  data.addColumn("number", "GX");
-  data.addColumn("number", "GY");
-  data.addColumn("number", "GZ");
+var leadsRef = database.ref("data");
 
-  data.addRows([
-    [1, 37.8, 80.8, 41.8],
-    [2, 30.9, 69.5, 32.4],
-    [3, 25.4, 57, 25.7],
-    [4, 11.7, 18.8, 10.5],
-    [5, 11.9, 17.6, 10.4],
-    [6, 8.8, 13.6, 7.7],
-    [7, 7.6, 12.3, 9.6],
-    [8, 12.3, 29.2, 10.6],
-    [9, 16.9, 42.9, 14.8],
-    [10, 12.8, 30.9, 11.6],
-    [11, 5.3, 7.9, 4.7],
-    [12, 6.6, 8.4, 5.2],
-    [13, 4.8, 6.3, 3.6],
-    [14, 4.2, 6.2, 3.4],
-  ]);
+leadsRef.on("value", function (snapshot) {
+  var rows = [];
 
-  var options = {
-    hAxis: {
-      title: "Time",
-    },
-    vAxis: {
-      title: "Value",
-    },
-    colors: ["#a52714", "#097138", "#ffff00"],
-    crosshair: {
-      color: "#000",
-      trigger: "selection",
-    },
-  };
+  snapshot.forEach(function (childSnapshot) {
+    var childData = childSnapshot.val();
+    var time = childSnapshot.key;
+    var fourPoints = childData.split(" ").map((point) => {
+      return parseInt(point.slice(3));
+    });
+    fourPoints.unshift(time);
 
-  var chart = new google.visualization.LineChart(
-    document.getElementById("chart_div")
-  );
+    rows.push(fourPoints);
+    drawTheGraph(rows);
+  });
+});
 
-  chart.draw(data, options);
-}
+let drawTheGraph = (rows) => {
+  google.charts.load("current", { packages: ["corechart", "line"] });
+  google.charts.setOnLoadCallback(drawCrosshairs);
+
+  function drawCrosshairs() {
+    var data = new google.visualization.DataTable();
+    data.addColumn("string", "X");
+    data.addColumn("number", "GX");
+    data.addColumn("number", "GY");
+    data.addColumn("number", "GZ");
+
+    data.addRows(rows);
+    console.log(rows);
+
+    var options = {
+      hAxis: {
+        title: "Time",
+      },
+      vAxis: {
+        title: "Value",
+      },
+      colors: ["#a52714", "#097138", "#ffff00"],
+      crosshair: {
+        color: "#000",
+        trigger: "selection",
+      },
+    };
+
+    var chart = new google.visualization.LineChart(
+      document.getElementById("chart_div")
+    );
+
+    chart.draw(data, options);
+  }
+};
